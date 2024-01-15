3dbf800c2c13bbf71e048b7140b44394458ac29e
dearfad <dearfad@gmail.com>
2024-01-15 21:47:53 +0800

    update

--- qabreast.py
+++ qabreast.py
@@ -4,8 +4,10 @@ from langchain.document_loaders import DirectoryLoader
 from langchain.embeddings.huggingface import HuggingFaceEmbeddings
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
 
 llm = Tongyi()
 loader = DirectoryLoader('./references/', glob="**/*.txt")
 docs = loader.load()
-st.write(docs[0][:200])
+st.write('Hello Breast!')
