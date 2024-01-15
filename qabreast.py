0726e72870d2ef936a80531f210b67b2e2e62977
dearfad <dearfad@gmail.com>
2024-01-15 22:23:16 +0800

    update

--- qabreast.py
+++ qabreast.py
@@ -8,17 +8,20 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 
-llm = Tongyi()
-loader = DirectoryLoader('./references/', glob="**/*.txt")
-docs = loader.load()
-embeddings = HuggingFaceEmbeddings(model_name='BAAI/bge-small-zh-v1.5')
-text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=32)
-documents = text_splitter.split_documents(docs)
-vector = FAISS.from_documents(documents, embeddings)
-prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:<context>{context}</context> Question: {input}""")
-document_chain = create_stuff_documents_chain(llm, prompt)
-retriever = vector.as_retriever()
-retrieval_chain = create_retrieval_chain(retriever, document_chain)
+@st.cache_resouces()
+def chain():
+    llm = Tongyi()
+    loader = DirectoryLoader('./references/', glob="**/*.txt")
+    docs = loader.load()
+    embeddings = HuggingFaceEmbeddings(model_name='BAAI/bge-small-zh-v1.5')
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=256, chunk_overlap=32)
+    documents = text_splitter.split_documents(docs)
+    vector = FAISS.from_documents(documents, embeddings)
+    prompt = ChatPromptTemplate.from_template("""Answer the following question based only on the provided context:<context>{context}</context> Question: {input}""")
+    document_chain = create_stuff_documents_chain(llm, prompt)
+    retriever = vector.as_retriever()
+    retrieval_chain = create_retrieval_chain(retriever, document_chain)
+    return retrieval_chain
 
 st.title('乳腺疾病专业问答')
 st.caption('参考：2022版中国乳腺癌随诊随访与健康管理指南')
