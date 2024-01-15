2328ad798c7d3446e30bc5701e835bda7abc72e1
dearfad <dearfad@gmail.com>
2024-01-15 22:36:14 +0800

    update

--- qabreast.py
+++ qabreast.py
@@ -8,8 +8,8 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
 
-@st.cache_resource(show_spinner=True)
-def get_chain():
+@st.cache_resource(show_spinner=False)
+def get_answer(prompt):
     llm = Tongyi()
     loader = DirectoryLoader('./references/', glob="**/*.txt")
     docs = loader.load()
@@ -21,13 +21,13 @@ def get_chain():
     document_chain = create_stuff_documents_chain(llm, prompt)
     retriever = vector.as_retriever()
     retrieval_chain = create_retrieval_chain(retriever, document_chain)
-    return retrieval_chain
+    response = retrieval_chain.invoke({"input": prompt})
+    return response["answer"]
 
 st.title('乳腺疾病专业问答')
 st.caption('参考：2022版中国乳腺癌随诊随访与健康管理指南')
 if prompt := st.text_input('请输入你的问题：', '子宫内膜增厚的标准是什么？'):
     answer_placeholder = st.empty()
     with st.spinner(text="增强检索中..."):
-        retrieval_chain = get_chain()
-        response = retrieval_chain.invoke({"input": prompt})
-    answer_placeholder.write(response["answer"])
+        answer = get_answer(prompt)
+    answer_placeholder.write(answer)
