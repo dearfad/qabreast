b9564e3f62eb7fc10b2a96287c1f5a9bb178e552
dearfad <dearfad@gmail.com>
2024-01-15 21:45:02 +0800

    update

--- qabreast.py
+++ qabreast.py
@@ -1,6 +1,10 @@
 import streamlit as st
 from langchain_community.llms import Tongyi
 from langchain.document_loaders import DirectoryLoader
+from langchain.embeddings.huggingface import HuggingFaceEmbeddings
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_community.vectorstores import FAISS
+
 llm = Tongyi()
 loader = DirectoryLoader('./references/', glob="**/*.txt")
 docs = loader.load()
