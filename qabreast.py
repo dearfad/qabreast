8a228a94c86d8921600bfdf64d3c83ab6903b70b
dearfad <dearfad@gmail.com>
2024-01-15 21:38:08 +0800

    update

--- qabreast.py
+++ qabreast.py
@@ -1,3 +1,7 @@
 import streamlit as st
-
-st.write('QaBreast')
+from langchain_community.llms import Tongyi
+from langchain.document_loaders import DirectoryLoader
+llm = Tongyi()
+loader = DirectoryLoader('./references/', glob="**/*.txt")
+docs = loader.load()
+st.write(docs[0][:200])
